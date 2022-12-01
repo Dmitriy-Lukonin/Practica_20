290d62ddd1708831ad49cc74d2aebcfbfd70d613
Dmitriy-Lukonin <lukonin.d.v@gmail.com>
2022-12-01 15:29:11 +0500

    Commit 1

--- Unit.18/bot/FirstBot.py
+++ Unit.18/bot/FirstBot.py
@@ -2,31 +2,42 @@ import telebot
 
 token = "5799376078:AAHNZx94tPIRx-OszvsoN5T1OTtGS8J-SUk"
 
-bot = telebot.Telebot(token)
+bot = telebot.TeleBot(token)
 
+keys = {
+    'Американский доллар': 'USD',
+    'Евро': 'EUR',
+    'Казахстанский тенге': 'KZT'
+}
+
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Привет, я умею конвертировать валюты.\
+Введи команду в следующем формате:\
+n<валюта>\
+<в какю валюту необходимо перевести>\
+<количество>\n Список валют: /values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def help(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key,))
+    bot.reply_to(message, text)
+
+
+bot.polling()
 
 # Обрабатываются все сообщения, содержащие команды '/start' or '/help'.
-@bot.message_handler(commands=['start', 'help'])
-def handle_start_help(message):
-    pass
-
-
-# Обрабатываются все документы и аудиозаписи
-@bot.message_handler(content_types=['document', 'audio'])
-def handle_docs_audio(message):
-    pass
-
-
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-
-
-bot.polling(none_stop=True, interval=0)
-
-
+# @bot.message_handler(commands=['start', 'help'])
+# def handle_start_help(message):
+#     pass
+#
+#
+# # Обрабатываются все документы и аудиозаписи
+# @bot.message_handler(content_types=['document', 'audio'])
+# def handle_docs_audio(message):
+#     pass
